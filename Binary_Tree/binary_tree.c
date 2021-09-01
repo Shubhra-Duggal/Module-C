d744306e8052f40a40f300313a2c1fd89b42fb41
sarvgyata <42912140+Shubhra-Duggal@users.noreply.github.com>
2021-09-01 13:07:22 +0530

    Update binary_tree.c

--- Binary_Tree/binary_tree.c
+++ Binary_Tree/binary_tree.c
@@ -10,8 +10,9 @@
 int main(){
     
     int array_input[] = {10, 20, 30};
-    struct tree_node *root = create_tree(array_input, 3, root, 0);
+    struct tree_node *root = create_binary_tree(array_input, 3);
     display_tree_inorder(root);
+    
     
     return 0;
 }
