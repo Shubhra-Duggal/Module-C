15b82c405916e1c3cec4221855a602f02fc68a81
sarvgyata <42912140+Shubhra-Duggal@users.noreply.github.com>
2021-09-01 13:07:04 +0530

    Update binary_tree.h

--- Binary_Tree/binary_tree.h
+++ Binary_Tree/binary_tree.h
@@ -16,17 +16,16 @@
  * Nodes would be constructed and linked to each other on the basis of certain rules
  */
 struct tree_node* create_tree_node(int data);
-struct tree_node* create_tree(int *data_elems, int len, struct tree_node *new_node, int num);
-void insert_tree_node(struct tree_node* root, int data);
+struct tree_node* create_binary_tree(int *data_elems, int len);
+struct tree_node* create_bst(int *data_elems, int len);
+struct tree_node* insert_tree_node(struct tree_node* root, int data);
 void display_tree_inorder(struct tree_node *root);
 void delete_tree_node(struct tree_node* root);
 
 /* ************************************************************************************** */
 
 struct tree_node* create_tree_node(int data){
-    struct tree_node* new_tree_node = NULL;
-    
-    new_tree_node = (struct tree_node*)malloc(sizeof(struct tree_node));
+    struct tree_node* new_tree_node = (struct tree_node*)malloc(sizeof(struct tree_node));
     new_tree_node -> data = data;
     new_tree_node -> left = NULL;
     new_tree_node -> right = NULL;
@@ -34,15 +33,45 @@ struct tree_node* create_tree_node(int data){
     return new_tree_node;
 }
 
-struct tree_node* create_tree(int *data_elems, int len, struct tree_node *new_node, int num){
-    if (num < len){
-        new_node = create_tree_node(*(data_elems + num));
+struct tree_node* insert_binary_tree(struct tree_node *root, int data){
+    if (root == NULL){
+        root = create_tree_node(data);
+        //printf("%d\n", root -> data);
+        return root;
+    }
+    else{
+        struct queue* tree_queue = create_queue(20);
+        enqueue(tree_queue, root);
         
-        new_node -> left = create_tree(data_elems, len, new_node -> left, 2*num + 1);
-        new_node -> right = create_tree(data_elems, len, new_node -> right, 2*num + 2);
+        while (!isEmpty(tree_queue)){
+            struct tree_node* old_node = dequeue(tree_queue);
+            
+            if (old_node -> left != NULL)
+                enqueue(tree_queue, old_node -> left);
+            else{
+                old_node -> left = create_tree_node(data);
+                return root;
+            }
+            
+            if (old_node -> right != NULL)
+                enqueue(tree_queue, old_node -> left);
+            else{
+                old_node -> right = create_tree_node(data);
+                return root;
+            }
+        }
+    }
+    return root;
+}
+
+struct tree_node* create_binary_tree(int *data_elems, int len){
+    struct tree_node *root = NULL;
+    
+    for (int i = 0; i < len; i++){
+        root = insert_binary_tree(root, *(data_elems + i));
     }
     
-    return new_node;
+    return root;
 }
 
 void display_tree_inorder(struct tree_node *root){
