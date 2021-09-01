0c0f8ba705c0f18bdd12ce22e9934cfcf6b241fe
sarvgyata <42912140+Shubhra-Duggal@users.noreply.github.com>
2021-09-01 14:31:49 +0530

    Update binary_tree.h

--- Binary_Tree/binary_tree.h
+++ Binary_Tree/binary_tree.h
@@ -11,6 +11,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "queue.h"
+#include "stack.h"
 /*
  * Implementation of the binary tree will be similar to linked list
  * Nodes would be constructed and linked to each other on the basis of certain rules
@@ -20,7 +21,10 @@ struct tree_node* create_binary_tree(int *data_elems, int len);
 struct tree_node* create_bst(int *data_elems, int len);
 struct tree_node* insert_tree_node(struct tree_node* root, int data);
 void display_tree_inorder(struct tree_node *root);
+void display_tree_preorder(struct tree_node *root);
+void display_tree_postorder(struct tree_node *root);
 void delete_tree_node(struct tree_node* root);
+int height_binary_tree(struct tree_node *root);
 
 /* ************************************************************************************** */
 
@@ -36,7 +40,6 @@ struct tree_node* create_tree_node(int data){
 struct tree_node* insert_binary_tree(struct tree_node *root, int data){
     if (root == NULL){
         root = create_tree_node(data);
-        //printf("%d\n", root -> data);
         return root;
     }
     else{
@@ -54,7 +57,7 @@ struct tree_node* insert_binary_tree(struct tree_node *root, int data){
             }
             
             if (old_node -> right != NULL)
-                enqueue(tree_queue, old_node -> left);
+                enqueue(tree_queue, old_node -> right);
             else{
                 old_node -> right = create_tree_node(data);
                 return root;
@@ -74,6 +77,7 @@ struct tree_node* create_binary_tree(int *data_elems, int len){
     return root;
 }
 
+
 void display_tree_inorder(struct tree_node *root){
     if (root != NULL){
         display_tree_inorder(root -> left);
@@ -82,5 +86,35 @@ void display_tree_inorder(struct tree_node *root){
     }
 }
 
+void display_tree_preorder(struct tree_node *root){
+    if (root != NULL){
+        printf("%d \n", root -> data);
+        display_tree_preorder(root -> left);
+        display_tree_preorder(root -> right);
+    }
+}
+
+void display_tree_postorder(struct tree_node *root){
+    if (root != NULL){
+        display_tree_preorder(root -> left);
+        display_tree_preorder(root -> right);
+        printf("%d \n", root -> data);
+    }
+}
+
+
+int height_binary_tree(struct tree_node *root){
+    if (root == NULL)
+        return 0;
+    
+    int left_h = height_binary_tree(root -> left);
+    int right_h = height_binary_tree(root -> right);
+    
+    if (left_h > right_h)
+        return left_h + 1;
+    else
+        return right_h + 1;
+    
+}
 
 #endif /* binary_tree_h */
